465bfb9275d92f580276f91b3253fa994f87c46e
AdaMerkush <merksa2@gmail.com>
2020-09-16 13:47:28 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import './js/unicorns';
+
+import './scss/base.scss';
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const { NODE_ENV = 'development' } = process.env;
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+};
